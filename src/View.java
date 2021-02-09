1c218c9c1d9ed26c2358198abcdf7e32f59b760a
NoKeverC <warger.jacob@gmail.com>
2021-02-09 16:48:06 +0100

    REDCAR.png temporary picture for experiment, view uppdated to generate panel and started to figure out how to rotate png file.

---
+++ src/View.java
@@ -0,0 +1,68 @@
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.io.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.LinkedList;
+
+import javax.imageio.*;
+import javax.swing.*;
+
+
+public class View extends JPanel {
+
+	private Model m;
+	static	BufferedImage redCar;
+	private LinkedList<Car> carList;
+	
+	public View(Model m) {
+		this.m = m;
+		this.carList = m.getCarList();
+		
+		try {
+		BufferedImage redCar = ImageIO.read(new File("REDCAR.png"));	
+		}
+		catch(IOException e){
+			
+		}
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		setBackground(Color.WHITE);
+		
+		Graphics2D g2d = (Graphics2D)g;
+		drawCar(g2d, carList.get(0));
+		
+	}
+	
+	
+	public void updateView() {
+		repaint();
+	}
+	
+	public void drawCar(Graphics2D g2d, Car car) {
+		
+		
+		//Make a backup so that we can reset our graphics object after using it.
+	    AffineTransform backup = g2d.getTransform();
+	    
+	    
+	    AffineTransform a = AffineTransform.getRotateInstance(car.getAngle() + (3.14/2), car.getXpos - w/2, getYpos - h/2);
+	  
+	    //Set our Graphics2D object to the transform
+	    g2d.setTransform(a);
+	    
+	    
+	    g2d.drawImage(redCar, x, y, null);
+	   
+	    //Reset our graphics object so we can draw with it again.
+	    g2d.setTransform(backup);
+	    
+		
+	}
+
+	
+}
