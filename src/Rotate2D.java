c3b413fbe9bf0f6dfe5af870508e4b723fb25557
kevinph00 <kevinpham@live.se>
2021-02-16 14:49:28 +0100

    Bugfix, lagt till 2 st debugklasser. Kommenterat changes

---
+++ src/Rotate2D.java
@@ -0,0 +1,11 @@
+class Rotate2D {
+
+    public static double getX(double x, double y, double angle) {
+        return x * Math.cos(angle) - y * Math.sin(angle);
+    }
+
+    public static double getY(double x, double y, double angle) {
+        return x * Math.sin(angle) + y * Math.cos(angle);
+    }
+
+}
