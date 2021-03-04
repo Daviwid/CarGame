56770f98f526ed32f319cd2beef554d83bb9b30e
vicboq <victoria.boquist@gmail.com>
2021-03-04 15:27:09 +0100

    har gjort overlapsWith metod mer kompakt

--- src/GameFiles/Model.java
+++ src/GameFiles/Model.java
@@ -191,19 +191,12 @@ public class Model implements Observable<Model>{
     	double height= (double)carList.get(0).getHeight()/10;
     	double carx= carList.get(0).getPositionX();
     	double cary= carList.get(0).getPositionY();
-    	double ax= carx - width;
-    	double ay= cary - height;
-    	double bx= carx + width;
-    	double by= cary - height;
-    	double cx= carx - width;  
-    	double cy= cary + height;
-    	double dx= carx + width;
-    	double dy= cary + height;
+    	
     	double rectarea = (2*height)*(2*width);
-    	double APD = Math.abs((px * ay - ax * py) + (dx * py - px * dy) + (ax * dy - dx * ay))/2;
-    	double DPC = Math.abs((px * dy - dx * py) + (cx * py - px * cy) + (dx * cy - cx * dy))/2;
-    	double CPB = Math.abs((px * cy - cx * py) + (bx * py - px * by) + (cx * by - bx * cy))/2;  
-    	double PBA = Math.abs((bx * py - px * by) + (ax * by - bx * ay) + (px * ay - ax * py))/2;
+    	double APD = Math.abs((px * (cary - height) - (carx - width) * py) + ((carx + width) * py - px * (cary + height)) + ((carx - width) * (cary + height) - (carx + width) * (cary - height)))/2;
+    	double DPC = Math.abs((px * (cary + height) - (carx + width) * py) + ((carx - width) * py - px * (cary + height)) + ((carx + width) * (cary + height) - (carx - width) * (cary + height)))/2;
+    	double CPB = Math.abs((px * (cary + height) - (carx - width) * py) + ((carx + width) * py - px * (cary - height)) + ((carx - width) * (cary - height) - (carx + width) * (cary + height)))/2;  
+    	double PBA = Math.abs(((carx + width) * py - px * (cary - height)) + ((carx - width) * (cary - height) - (carx + width) * (cary - height)) + (px * (cary - height) - (carx - width) * py))/2;
     	double sum = APD + DPC + CPB + PBA; 
     	
     	if(sum > rectarea ) return false;  
