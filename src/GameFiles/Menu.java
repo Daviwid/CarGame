46c48481f3ec81e2010553902e1a4cc1744789fc
kevinph00 <kevinpham@live.se>
2021-03-05 11:26:42 +0100

    Enkel klassjavadoc på menu

--- src/GameFiles/Menu.java
+++ src/GameFiles/Menu.java
@@ -7,7 +7,10 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-
+/**
+ * Oversees the components of the menu UI that is going to be drawn.
+ * 
+ */
 public class Menu
 {
     private Rectangle playBtn, quitBtn, configBtn,
